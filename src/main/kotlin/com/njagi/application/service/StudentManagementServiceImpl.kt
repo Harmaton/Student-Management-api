561d85fafe91c1c964c79447de779d9e3b733ce4
Harmaton <njagiiharmaton@gmail.com>
2022-08-25 21:57:22 +0300

    repository completed

--- src/main/kotlin/com/njagi/application/service/StudentManagementServiceImpl.kt
+++ src/main/kotlin/com/njagi/application/service/StudentManagementServiceImpl.kt
@@ -4,10 +4,12 @@ import com.njagi.application.dao.StudentDao
 import com.njagi.application.dto.AddStudentRequest
 import com.njagi.application.dto.StudentResponse
 import com.njagi.application.dto.UpdateStudentRequest
+import com.njagi.application.transformer.AddStudentRequestTransformer
 import org.springframework.stereotype.Service
 
 @Service
-class PersonManagementServiceImpl(private val studentDao: StudentDao) : PersonManagementService {
+class PersonManagementServiceImpl(private val studentDao: StudentDao,
+                                  private val addStudentRequestTransformer: AddStudentRequestTransformer) : PersonManagementService {
     override fun findById(id: String): StudentResponse? {
         TODO("Not yet implemented")
     }
---
+++ src/main/kotlin/com/njagi/application/transformer/StudentResponseTransformerExtension.kt
@@ -0,0 +1,2 @@
+package com.njagi.application.transformer
+
