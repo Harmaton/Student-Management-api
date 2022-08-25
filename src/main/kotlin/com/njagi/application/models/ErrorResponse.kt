de99991d0e058411281b22bbb54afad301dcbf5b
Harmaton <njagiiharmaton@gmail.com>
2022-08-25 23:08:06 +0300

    bad request handling

---
+++ src/main/kotlin/com/njagi/application/models/ErrorResponse.kt
@@ -0,0 +1,3 @@
+package com.njagi.application.models
+
+data class ErrorResponse()
--- src/main/kotlin/com/njagi/application/models/Student.kt
+++ src/main/kotlin/com/njagi/application/models/Student.kt
@@ -1,17 +1,16 @@
 package com.njagi.application.models
 
 import java.time.Year
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.SequenceGenerator
+import javax.persistence.*
 
 @Entity
 data class Student(@Id
                    @SequenceGenerator(name = PERSON_SEQUENCE, sequenceName = PERSON_SEQUENCE, initialValue = 1, allocationSize = 1)
-                   var id: String?,
-                   var name: String? = null,
-                   val course: String? = null,
-                   val year: Year? = null){
+                   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = PERSON_SEQUENCE)
+                   var id: String = "",
+                   var name: String? = "",
+                   var course: String? = ""
+                   ){
 
     companion object{
       const val PERSON_SEQUENCE: String = "PERSON_SEQUENCE"
