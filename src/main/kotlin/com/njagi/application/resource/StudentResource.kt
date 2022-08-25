1538bf6cebfb2cd4b52d4ad1e7ff75e2cd7503c5
Harmaton <njagiiharmaton@gmail.com>
2022-08-25 22:25:50 +0300

    api mappers completed

--- src/main/kotlin/com/njagi/application/resource/StudentResource.kt
+++ src/main/kotlin/com/njagi/application/resource/StudentResource.kt
@@ -1,4 +1,14 @@
 package com.njagi.application.resource
 
-class StudentResource {
+import com.njagi.application.dto.AddStudentRequest
+import com.njagi.application.dto.StudentResponse
+import com.njagi.application.dto.UpdateStudentRequest
+import org.springframework.http.ResponseEntity
+
+interface StudentResource {
+    fun findById(id: String): ResponseEntity<StudentResponse?>
+    fun findAll(): ResponseEntity<List<StudentResponse>>
+    fun save(addStudentRequest: AddStudentRequest): ResponseEntity<StudentResponse>
+    fun update(updateStudentRequest: UpdateStudentRequest): ResponseEntity<StudentResponse>
+    fun deleteById(id:String): ResponseEntity<Unit>
 }
